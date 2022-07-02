14ccbeba441171a1c455258b07b9ff68ca23a14a
Saad Khalid <saadkhalid913@gmail.com>
2022-07-02 12:08:00 -0400

    Added stuff

--- socketio/index.ts
+++ socketio/index.ts
@@ -1,8 +1,12 @@
-import { Server} from "socket.io"
+import { Server } from "socket.io"
 import { Express } from "express"
 import http from "http"
+import prisma from "../db"
 
-export default function SocketIOinit(app: Express) {
+
+
+export default function SocketIOinit(app: Express) {        
+
     const server = http.createServer(app);
     const io = new Server(server,{cors: {
         origin: "http://localhost:3001",
@@ -10,12 +14,21 @@ export default function SocketIOinit(app: Express) {
       }})
 
     io.on('connection', (socket) => {
-
-
         socket.on("test-event" , (...args) => {
             console.log("This is the test event")
             console.log(args)
         })
+
+
+        // socket.on("chat", async (sender, text) => {
+        //     const ChatSendResponse = await prisma.chat.create({
+        //         data: {
+        //             sender,
+        //             text,
+        //             recipient:
+        //         }
+        //     })  
+        // })
     });
 
     return server 
